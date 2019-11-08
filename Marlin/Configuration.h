2908b7c0fddc6ce39995beece0f85d0e6ce115ee
Per A. Toft <per@minfejl.dk>
2019-11-08 10:06:05 +0100

    uPDATED PID AND E-STEP

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -473,10 +473,15 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
+   // PTO CR-10S @195C
+   #define DEFAULT_Kp 37.02
+   #define DEFAULT_Ki 3.86
+   #define DEFAULT_Kd 88.71
+
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  // #define DEFAULT_Kp 22.2
+  // #define DEFAULT_Ki 1.08
+  // #define DEFAULT_Kd 114
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
@@ -523,11 +528,16 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
+  //PTO CR-10S BED @55C
+  #define DEFAULT_bedKp 143.73
+  #define DEFAULT_bedKi 27.85
+  #define DEFAULT_bedKd 185.44
+
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  // #define DEFAULT_bedKp 10.00
+  // #define DEFAULT_bedKi .023
+  // #define DEFAULT_bedKd 305.4
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
@@ -716,7 +726,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 95 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 98.44 }
 
 /**
  * Default Max Feed Rate (mm/s)
