7d99c1b654fef126984d12053b9c93d017969e11
Per A. Toft <per@minfejl.dk>
2020-04-14 14:47:11 +0200

    Enabled Actions for runout sensor

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2879,9 +2879,10 @@
  * Host Prompt Support enables Marlin to use the host for user prompts so
  * filament runout and other processes can be managed from the host side.
  */
-//#define HOST_ACTION_COMMANDS
+// PTO: Must be enabled for fillament run-out sensor to work with Octoprint
+#define HOST_ACTION_COMMANDS
 #if ENABLED(HOST_ACTION_COMMANDS)
-  //#define HOST_PROMPT_SUPPORT
+  #define HOST_PROMPT_SUPPORT
 #endif
 
 /**
