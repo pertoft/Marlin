581d42b6663f1e9e18301815bd14569c847b5731
Per Abildgaard Toft <per@minfejl.dk>
2020-04-17 10:06:03 +0200

    Hævet antal af probe punkter fra 3 til 5

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1273,8 +1273,9 @@
 
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
+  // PTO: Hævet fra 3 punkter til 5 punkter da bed er meget ujævn
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 5
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
